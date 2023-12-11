8b505c907714f637ba48fecef125e4fdf9850d59
sammifs <samuel.sverker@gmail.com>
2023-12-11 09:56:57 +0100

    doc

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# ioopm_inlupp3
+# INLUPP3 #
+Symbolic calculator, works like a simple interactive programming language. 4 + 36, Sin(-123), 25 = x, etc.
+
+
+# Run
+To run this program simply write "make all" followed by "make run".
+
+# Packages
+This program is made up of three packages.
+
+* org.ioopm.calculator.ast
+* org.ioopm.calculator.parser
+* org.ioopm.calculator
+
+# Types and subtypes
+
+SymbolicExpression/
+├─ Binary/
+│  ├─ Addition
+│  ├─ Subtraction
+│  ├─ Multiplication
+│  ├─ Division
+│  ├─ Assignment
+├─ Unary/
+│  ├─ Sin
+│  ├─ Cos
+│  ├─ Exp
+│  ├─ Log
+│  ├─ Negation
+├─ Atom/
+│  ├─ Variable
+│  ├─ Constant
+├─ Command/
+│  ├─ Vars
+│  ├─ Quit
+│  ├─ Clear
+
--- src/org/ioopm/calculator/Calculator.java
+++ src/org/ioopm/calculator/Calculator.java
@@ -8,10 +8,12 @@ import org.ioopm.calculator.parser.IllegalExpressionException;
 import org.ioopm.calculator.parser.SyntaxErrorException;
 
 public class Calculator {
+    // Program main
     public static void main(String[] args) throws IOException {
         final CalculatorParser cp = new CalculatorParser();
         final Environment vars = new Environment();
 
+        // Info for print upon system exit.
         int expressions = 0;
         int success = 0;
         int full_evaluations = 0;
@@ -21,6 +23,7 @@ public class Calculator {
             try {
                 SymbolicExpression se = cp.parse(input, vars);
                 expressions++;
+                // Handle Command seperately.
                 if (se.isCommand()) {
                     if (se instanceof Quit) {
                         break;
@@ -31,17 +34,21 @@ public class Calculator {
                     }
                 } else {
                     try { 
+                        // eval and print
                         SymbolicExpression ans = se.eval(vars);
                         System.out.println(ans);
                         success++;
+                        // eval is considered "full" if it produces Constant
                         if (ans.isConstant()) {
                             full_evaluations++;
                         }
                     } catch (IllegalAssignmentException e) {
+                        // IllegalAssignment is not fatal, catch and move on.
                         System.out.println(e);
                     }
                 }
             } catch (SyntaxErrorException e) {
+                // SyntaxError & IllegalExpression have to be caught before eval.
                 System.out.println(e);
             } catch (IllegalExpressionException e) {
                 System.out.println(e);
